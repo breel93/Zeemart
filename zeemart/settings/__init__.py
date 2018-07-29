a8c423d76d39f766d043d3a77cce9c5fcbc8427b
breezil <kolajubril@gmail.com>
2018-07-29 08:26:10 -0400

    update project settings

--- zeemart/settings/__init__.py
+++ zeemart/settings/__init__.py
@@ -1,12 +1,12 @@
 from .base import *
 
 
-from .development import *
+# from .development import *
 
 from .production import *
 
-try:
-    from .development import *
-except:
-    pass
+# try:
+#     from .development import *
+# except:
+#     pass
 
