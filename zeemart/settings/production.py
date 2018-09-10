09b5e327ed59959f74ab433a932b72de2004829b
breezil <kolajubril@gmail.com>
2018-09-10 08:18:33 -0400

    heroku please work

--- zeemart/settings/production.py
+++ zeemart/settings/production.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    # 'django.contrib.auth',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
