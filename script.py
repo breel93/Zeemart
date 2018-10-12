2630305d6510df777347d67e69e5ac168ee5d892
breezil <kolajubril@gmail.com>
2018-10-12 06:22:23 -0400

    product data

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import os
 
-with open('products/data/products.json', 'rb') as f:
+with open('prod.json', 'rb') as f:
     data = f.read()
 newdata = open('products.json', 'wb')
 newdata.write(data)
